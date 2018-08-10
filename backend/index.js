e00481b7cc7c2b71916df0d743b91e68afc12363
Bruno Krebs <bruno.krebs@auth0.com>
2018-08-09 22:25:42 -0300

    ## Creating the Backend API

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const cors = require('cors');
+const express = require('express');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const envVariables = require('./env-variables.json');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/public', (req, res) => res.send('Everyone in the world can read this message.'));
+
+app.use(jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${envVariables.auth0Domain}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: envVariables.apiIdentifier,
+  issuer: `https://${envVariables.auth0Domain}/`,
+  algorithms: ['RS256']
+}));
+
+app.get('/private', (req, res) => res.send('Only authenticated users can read this message.'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
